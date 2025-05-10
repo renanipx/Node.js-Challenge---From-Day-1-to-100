10d13a813f9430bcf264a2e9fbe8fff407a204e6
renanipx <137443957+renanipx@users.noreply.github.com>
2025-05-10 19:16:13 -0300

    feat: Set up OpenTelemetry with Jaeger for distributed tracing in Node.js application

---
+++ node-challenge - day 79/app.js
@@ -0,0 +1,13 @@
+require('./tracing'); // <- Must be loaded before anything else
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Distributed Tracing!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
